b1a491e25d1fc44b1df67d13600eeea8170b4de0
Crowdbotics <team@crowdbotics.com>
2020-06-12 16:15:32 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/autolayout4456_dev_5941/MainActivity.java
@@ -0,0 +1,15 @@
+package com.autolayout4456_dev_5941;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "autolayout4456_dev_5941";
+    }
+}
